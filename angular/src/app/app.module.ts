4c0b07de84a20b7d3f65b8e3b4f4be56fbd229f3
akilisosa <akilsosa@gmail.com>
2019-07-16 12:44:35 -0400

    yeah this is kind of cool

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrameComponent } from './frame/frame.component';
+import { MainChatComponent } from './frame/main-chat/main-chat.component';
+import { UserInfoComponent } from './frame/user-info/user-info.component';
+import { FriendsComponent } from './frame/friends/friends.component';
+import { PrivateMessageComponent } from './frame/private-message/private-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrameComponent,
+    MainChatComponent,
+    UserInfoComponent,
+    FriendsComponent,
+    PrivateMessageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
